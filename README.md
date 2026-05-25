4c47f4ee28d240db2f3096c6dd1d2aeb9d684745
Makoto Shimodate <zeon675@gmail.com>
2026-05-25 18:18:46 +0900

    composer lockを追加する

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PHPで本のバーコードを読み取れる
+https://radiant-peak-35964-0dbe5c5c2b74.herokuapp.com/ | https://git.heroku.com/radiant-peak-35964.git
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '363b46cd73f713889c55f2e783195b33f585f2af',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '363b46cd73f713889c55f2e783195b33f585f2af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
